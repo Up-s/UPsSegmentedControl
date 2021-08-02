a0ae752daba41b40da12a0c23713c8a72ad5829e
Up-s <dldbdjq@gmail.com>
2021-08-02 10:38:06 +0900

    update podspec

---
+++ Example/Pods/Target Support Files/Pods-UPsSegmentedControl_Tests/Pods-UPsSegmentedControl_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UPsSegmentedControl_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_UPsSegmentedControl_Tests
+@end
---
+++ Example/Pods/Target Support Files/UPsSegmentedControl/UPsSegmentedControl-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_UPsSegmentedControl : NSObject
+@end
+@implementation PodsDummy_UPsSegmentedControl
+@end
